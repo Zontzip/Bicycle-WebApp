f21d7f25da315ea4b62e1744f8823778d1d55bd7
Zontzor <alex.kiernan@mydit.ie>
2015-12-05 23:19:42 +0000

    Added order service

---
+++ src/com/example/service/OrderService.java
@@ -0,0 +1,24 @@
+package com.example.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.business.Order;
+import com.example.dao.OrderDao;
+import com.example.exceptions.DaoException;
+
+public class OrderService {
+	OrderDao dao = new OrderDao();
+	
+	public List<Order> getAllUsers() {
+		List<Order> orders = new ArrayList<Order>();
+		
+		try {
+			orders = dao.getAllOrders();
+		} 
+		catch (DaoException e) {
+			e.printStackTrace();
+		}
+		return orders;
+	}
+}
